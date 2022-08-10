3ab58faff0763dc8723b16a027db32f1bf9f0cab
JohnCedrick-01 <cedrick@usource.me>
2022-08-10 15:35:02 +0800

    98% onboard

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './contact.css'
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {SiViber} from 'react-icons/si'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_2q6heid', 'template_sshev4j', form.current, '9uzYtuOnmBbRgn7gI')
+        e.target.reset()
+    };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <HiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>sirjc.andal@gmail.com</h5>
+            <a href="mailto:johncedrick.magtibay01@gmail.com" target="_blank">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>John Cedrick Andal Magtibay</h5>
+            <a href="https://m.me/sirjc001" target="_blank">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <SiViber className='contact__option-icon'/>
+            <h4>Viber</h4>
+            <h5>+63912-080-0728</h5>
+            <a href="https:viber.com" target="_blank">Send a Message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+            <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+        
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {AiOutlineFacebook} from 'react-icons/ai'
+import {FiTwitter} from 'react-icons/fi'
+import {BsInstagram} from 'react-icons/bs'
 
-function Footer() {
+const Footer= () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href="#" class='footer__logo'>JOHN-CEDRICK-MAGTIBAY</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contacts">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"> <AiOutlineFacebook/> </a>
+        <a href="https://twitter.com"> <FiTwitter/> </a>
+        <a href="https://instagram.com"> <BsInstagram/> </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; John Cedrick Magtibay. All right reserved.</small>
+      </div>
+    </footer>
   )
 }
 
